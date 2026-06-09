e4ce71b04ca27f3907bbf11204b20a7674461a21
Mrr Hak <longkimhak.kh@gmail.com>
2026-06-09 21:12:15 +0700

    fix(ios): add FlutterFramework dependency and improve tests
      - Add FlutterFramework package dependency and target linkage to Package.swift
      - Enable PrivacyInfo.xcprivacy resource bundling
      - Replace empty RunnerTests placeholder with meaningful XCTest coverage

--- example/ios/RunnerTests/RunnerTests.swift
+++ example/ios/RunnerTests/RunnerTests.swift
@@ -1,29 +1,75 @@
 import Flutter
 import UIKit
 import XCTest
-
-// If your plugin has been explicitly set to "type: .dynamic" in the Package.swift,
-// you will need to add your plugin as a dependency of RunnerTests within Xcode.
-
 @testable import screen_lock_detector
-
-// This demonstrates a simple unit test of the Swift portion of this plugin's implementation.
-//
-// See https://developer.apple.com/documentation/xctest for more information about using XCTest.
 
 class RunnerTests: XCTestCase {
 
-  func testGetPlatformVersion() {
+  func testCheckScreenStatus_returnsUnlockedByDefault() {
+    let plugin = ScreenLockDetectorPlugin()
+    let call = FlutterMethodCall(methodName: "checkScreenStatus", arguments: nil)
+    var result: Any?
+
+    plugin.handle(call) { value in result = value }
+
+    XCTAssertEqual(result as? String, "UNLOCKED")
+  }
+
+  func testUnknownMethod_returnsNotImplemented() {
+    let plugin = ScreenLockDetectorPlugin()
+    let call = FlutterMethodCall(methodName: "unknownMethod", arguments: nil)
+    var result: Any?
+
+    plugin.handle(call) { value in result = value }
+
+    XCTAssertTrue((result as AnyObject) === (FlutterMethodNotImplemented as AnyObject))
+  }
+
+  func testDeviceWillLock_setsStatusToLocked() {
     let plugin = ScreenLockDetectorPlugin()
 
-    let call = FlutterMethodCall(methodName: "getPlatformVersion", arguments: [])
+    plugin.deviceWillLock()
 
-    let resultExpectation = expectation(description: "result block must be called.")
-    plugin.handle(call) { result in
-      XCTAssertEqual(result as! String, "iOS " + UIDevice.current.systemVersion)
-      resultExpectation.fulfill()
-    }
-    waitForExpectations(timeout: 1)
+    let call = FlutterMethodCall(methodName: "checkScreenStatus", arguments: nil)
+    var result: Any?
+    plugin.handle(call) { value in result = value }
+
+    XCTAssertEqual(result as? String, "LOCKED")
+  }
+
+  func testDeviceDidUnlock_setsStatusBackToUnlocked() {
+    let plugin = ScreenLockDetectorPlugin()
+    plugin.deviceWillLock()
+
+    plugin.deviceDidUnlock()
+
+    let call = FlutterMethodCall(methodName: "checkScreenStatus", arguments: nil)
+    var result: Any?
+    plugin.handle(call) { value in result = value }
+
+    XCTAssertEqual(result as? String, "UNLOCKED")
+  }
+
+  func testDeviceWillLock_doesNotFireTwice() {
+    let plugin = ScreenLockDetectorPlugin()
+    var sinkCallCount = 0
+    plugin.onListen(withArguments: nil) { _ in sinkCallCount += 1 }
+
+    plugin.deviceWillLock()
+    plugin.deviceWillLock()
+
+    XCTAssertEqual(sinkCallCount, 1)
+  }
+
+  func testDeviceDidUnlock_doesNotFireTwiceWhenAlreadyUnlocked() {
+    let plugin = ScreenLockDetectorPlugin()
+    var sinkCallCount = 0
+    plugin.onListen(withArguments: nil) { _ in sinkCallCount += 1 }
+
+    plugin.deviceDidUnlock()
+    plugin.deviceDidUnlock()
+
+    XCTAssertEqual(sinkCallCount, 0)
   }
 
 }
--- ios/screen_lock_detector/Package.swift
+++ ios/screen_lock_detector/Package.swift
@@ -11,17 +11,21 @@ let package = Package(
     products: [
         .library(name: "screen-lock-detector", targets: ["screen_lock_detector"])
     ],
-    dependencies: [],
+    dependencies: [
+        .package(name: "FlutterFramework", path: "../FlutterFramework")
+    ],
     targets: [
         .target(
             name: "screen_lock_detector",
-            dependencies: [],
+            dependencies: [
+                .product(name: "FlutterFramework", package: "FlutterFramework")
+            ],
             resources: [
                 // If your plugin requires a privacy manifest, for example if it uses any required
                 // reason APIs, update the PrivacyInfo.xcprivacy file to describe your plugin's
                 // privacy impact, and then uncomment these lines. For more information, see
                 // https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
-                // .process("PrivacyInfo.xcprivacy"),
+                .process("PrivacyInfo.xcprivacy"),
 
                 // If you have other resources that need to be bundled with your plugin, refer to
                 // the following instructions to add them:
